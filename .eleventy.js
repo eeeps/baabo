5bba01dc2649190a80efefa16bd746f0a65cb3b8
Eric Portis <e@ericportis.com>
2023-06-17 13:07:55 -0700

    Oops need to pass through _redirects

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,5 @@
 module.exports = function( eleventyConfig ) {
 	eleventyConfig.addPassthroughCopy( 'style.css' );
 	eleventyConfig.addPassthroughCopy( 'baabo.js' );
+	eleventyConfig.addPassthroughCopy( '_redirects' );
 }
