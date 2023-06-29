3fea3dea8fe920dc3f5528ce9d097e86fe9b886d
Eric Portis <e@ericportis.com>
2023-06-28 20:35:52 -0700

    work file for who-got-each-challenge

--- create-who-got-each-challenge.js
+++ create-who-got-each-challenge.js
@@ -20,12 +20,27 @@ const challengesPlayers = boards.reduce( ( challengesPlayersMap, board ) => {
 }, new Map() );
 
 // console.log(challengesPlayers)
+// 
 
+// console.log a dl (lol)
 console.log('<dl>');
 challengesPlayers.forEach( ( players, challenge ) => console.log(`
+<div>
 <dt>${challenge}</dt>
 ${
 	players.map( p => `<dd>${p}</dd>`).join('\n')
 }
+</div>
 `))
 console.log('</dl>');
+
+// see if there were any challenges that weren't picked by any player
+
+// const pickedChallenges = [...challengesPlayers.keys()];
+// const allChallenges = JSON.parse( 
+// 	fs.readFileSync('_data/challenges.json')
+// );
+// const diff = allChallenges.filter(x => !pickedChallenges.includes(x));
+// console.log(allChallenges.length);
+// console.log(pickedChallenges.length)
+//console.log(diff)
