1e8fda6b5f1b3b9c3fe1c5505cbe69d928327ef7
Eric Portis <e@ericportis.com>
2023-06-28 20:39:09 -0700

    Add Julie

--- who-got-each-challenge.11ty.js
+++ who-got-each-challenge.11ty.js
@@ -49,142 +49,182 @@ dt {
 	</header>
 	
 	<h2>Who got each challenge?</h2>
-
 <dl>
 
-<div><dt>Bike on a pump track</dt>
+<div>
+<dt>Bike on a pump track</dt>
 <dd>Britt</dd>
 <dd>Lauren</dd>
 <dd>Jason</dd>
 <dd>Jasper</dd>
 <dd>Caity</dd>
+<dd>Julie</dd>
+</div>
 
 
-</div><div><dt>Find a queer or BIPOC owned resturant and give them a good Yelp review</dt>
+<div>
+<dt>Find a queer or BIPOC owned resturant and give them a good Yelp review</dt>
 <dd>Britt</dd>
 <dd>Lauren</dd>
 <dd>Jason</dd>
 <dd>Caity</dd>
 <dd>Bryon</dd>
+<dd>Julie</dd>
+</div>
 
 
-</div><div><dt>Do a BAABO activity that you wanted to do that didn't end up on your board</dt>
+<div>
+<dt>Do a BAABO activity that you wanted to do that didn't end up on your board</dt>
 <dd>Britt</dd>
 <dd>Lauren</dd>
 <dd>Jake</dd>
 <dd>Jason</dd>
 <dd>Caity</dd>
 <dd>Bryon</dd>
+</div>
 
 
-</div><div><dt>Glean a pound of stone fruit</dt>
+<div>
+<dt>Glean a pound of stone fruit</dt>
 <dd>Britt</dd>
 <dd>Jamie</dd>
 <dd>Jasper</dd>
 <dd>Megan</dd>
 <dd>Eric</dd>
+</div>
 
 
-</div><div><dt>Make a playlist of your all-time favorite summer jams</dt>
+<div>
+<dt>Make a playlist of your all-time favorite summer jams</dt>
 <dd>Britt</dd>
 <dd>Lauren</dd>
 <dd>Andrea</dd>
 <dd>Jasper</dd>
 <dd>Bryson</dd>
 <dd>Ellie</dd>
+</div>
 
 
-</div><div><dt>Start a BAABO scrapbook with other folks who get this challenge</dt>
+<div>
+<dt>Start a BAABO scrapbook with other folks who get this challenge</dt>
 <dd>Britt</dd>
 <dd>Jasper</dd>
 <dd>Celeste</dd>
 <dd>Megan</dd>
 <dd>Mary</dd>
 <dd>Caity</dd>
+<dd>Julie</dd>
+</div>
 
 
-</div><div><dt>Go roller skating</dt>
+<div>
+<dt>Go roller skating</dt>
 <dd>Britt</dd>
 <dd>Jake</dd>
 <dd>Bryson</dd>
+</div>
 
 
-</div><div><dt>Go to three or more breweries in one day</dt>
+<div>
+<dt>Go to three or more breweries in one day</dt>
 <dd>Britt</dd>
 <dd>Lauren</dd>
 <dd>Jason</dd>
 <dd>Mary</dd>
+<dd>Julie</dd>
+</div>
 
 
-</div><div><dt>Come up with a three minute synchronized swimming routine</dt>
+<div>
+<dt>Come up with a three minute synchronized swimming routine</dt>
 <dd>Britt</dd>
 <dd>Lauren</dd>
 <dd>Jake</dd>
 <dd>Jasper</dd>
 <dd>Celeste</dd>
 <dd>Bryson</dd>
+</div>
 
 
-</div><div><dt>Meet a llama in person</dt>
+<div>
+<dt>Meet a llama in person</dt>
 <dd>Britt</dd>
 <dd>Celeste</dd>
 <dd>Megan</dd>
 <dd>Caity</dd>
+</div>
 
 
-</div><div><dt>Eat a hot dog at a baseball game</dt>
+<div>
+<dt>Eat a hot dog at a baseball game</dt>
 <dd>Britt</dd>
 <dd>Andrea</dd>
 <dd>Mary</dd>
 <dd>Bryon</dd>
+<dd>Julie</dd>
+</div>
 
 
-</div><div><dt>Visit a new to you city, county, and state park</dt>
+<div>
+<dt>Visit a new to you city, county, and state park</dt>
 <dd>Britt</dd>
 <dd>Jason</dd>
 <dd>Mary</dd>
+<dd>Julie</dd>
+</div>
 
 
-</div><div><dt>Record and share an episode of drunk history but the Bellingham version</dt>
+<div>
+<dt>Record and share an episode of drunk history but the Bellingham version</dt>
 <dd>Britt</dd>
 <dd>Andrea</dd>
 <dd>Jasper</dd>
 <dd>Ellie</dd>
 <dd>Caity</dd>
 <dd>Bryon</dd>
+</div>
 
 
-</div><div><dt>Pick one other BAABO player and coordinate your outfits so you're matching for a day</dt>
+<div>
+<dt>Pick one other BAABO player and coordinate your outfits so you're matching for a day</dt>
 <dd>Britt</dd>
 <dd>Ellie</dd>
+</div>
 
 
-</div><div><dt>Find a geocache and leave a message in it</dt>
-<dd>Britt</dd>
-<dd>Andrea</dd>
-<dd>Jason</dd>
-<dd>Ellie</dd>
-<dd>Eric</dd>
-
-
-</div><div><dt>Go with somebody and jump off Taylor Dock</dt>
-<dd>Britt</dd>
-<dd>Lauren</dd>
-<dd>Celeste</dd>
-<dd>Bryson</dd>
-<dd>Megan</dd>
-<dd>Eric</dd>
-
-
-</div><div><dt>Spend a night solo retreating somewhere</dt>
+<div>
+<dt>Find a geocache and leave a message in it</dt>
 <dd>Britt</dd>
 <dd>Andrea</dd>
 <dd>Jason</dd>
+<dd>Ellie</dd>
+<dd>Eric</dd>
+</div>
+
+
+<div>
+<dt>Go with somebody and jump off Taylor Dock</dt>
+<dd>Britt</dd>
+<dd>Lauren</dd>
+<dd>Celeste</dd>
+<dd>Bryson</dd>
+<dd>Megan</dd>
+<dd>Eric</dd>
+</div>
+
+
+<div>
+<dt>Spend a night solo retreating somewhere</dt>
+<dd>Britt</dd>
+<dd>Andrea</dd>
+<dd>Jason</dd>
 <dd>Megan</dd>
 <dd>Mary</dd>
+</div>
 
 
-</div><div><dt>Selfie with a geoduck</dt>
+<div>
+<dt>Selfie with a geoduck</dt>
 <dd>Britt</dd>
 <dd>Lauren</dd>
 <dd>Andrea</dd>
@@ -193,16 +233,20 @@ dt {
 <dd>Mary</dd>
 <dd>Caity</dd>
 <dd>Eric</dd>
+</div>
 
 
-</div><div><dt>Set up a friend (friendship OK!)</dt>
+<div>
+<dt>Set up a friend (friendship OK!)</dt>
 <dd>Britt</dd>
 <dd>Jamie</dd>
 <dd>Celeste</dd>
 <dd>Ellie</dd>
+</div>
 
 
-</div><div><dt>Host a themed dinner party</dt>
+<div>
+<dt>Host a themed dinner party</dt>
 <dd>Britt</dd>
 <dd>Lauren</dd>
 <dd>Jamie</dd>
@@ -212,16 +256,20 @@ dt {
 <dd>Caity</dd>
 <dd>Bryon</dd>
 <dd>Eric</dd>
+</div>
 
 
-</div><div><dt>Meet and greet five new to you dogs (ask first!)</dt>
+<div>
+<dt>Meet and greet five new to you dogs (ask first!)</dt>
 <dd>Britt</dd>
 <dd>Jake</dd>
 <dd>Bryson</dd>
 <dd>Eric</dd>
+</div>
 
 
-</div><div><dt>Complete a needle-point project</dt>
+<div>
+<dt>Complete a needle-point project</dt>
 <dd>Britt</dd>
 <dd>Lauren</dd>
 <dd>Jason</dd>
@@ -229,200 +277,258 @@ dt {
 <dd>Ellie</dd>
 <dd>Caity</dd>
 <dd>Bryon</dd>
+</div>
 
 
-</div><div><dt>Go to a Bellingham City Countil meeting</dt>
+<div>
+<dt>Go to a Bellingham City Countil meeting</dt>
 <dd>Britt</dd>
 <dd>Lauren</dd>
 <dd>Jason</dd>
 <dd>Jamie</dd>
 <dd>Jasper</dd>
 <dd>Mary</dd>
+<dd>Julie</dd>
+</div>
 
 
-</div><div><dt>Eat a slice of apple pie with cheddar cheese</dt>
+<div>
+<dt>Eat a slice of apple pie with cheddar cheese</dt>
 <dd>Britt</dd>
 <dd>Lauren</dd>
 <dd>Mary</dd>
 <dd>Ellie</dd>
+</div>
 
 
-</div><div><dt>Write a rap about BAABO (at least 10 lines) and perform to the group</dt>
+<div>
+<dt>Write a rap about BAABO (at least 10 lines) and perform to the group</dt>
 <dd>Lauren</dd>
 <dd>Celeste</dd>
 <dd>Caity</dd>
+</div>
 
 
-</div><div><dt>Drive to Winthrop on Hwy 20 (spend at least some of the time driving)</dt>
+<div>
+<dt>Drive to Winthrop on Hwy 20 (spend at least some of the time driving)</dt>
 <dd>Lauren</dd>
 <dd>Jason</dd>
 <dd>Bryson</dd>
 <dd>Caity</dd>
 <dd>Bryon</dd>
+</div>
 
 
-</div><div><dt>Get an astrology or tarot reading done by a professional</dt>
-<dd>Lauren</dd>
-<dd>Andrea</dd>
-<dd>Jamie</dd>
-<dd>Celeste</dd>
-
-
-</div><div><dt>Participate in a sun dress bar crawl</dt>
-<dd>Lauren</dd>
-<dd>Jake</dd>
-<dd>Jasper</dd>
-<dd>Megan</dd>
-
-
-</div><div><dt>Attend or volunteer at an event with a queer allied organization</dt>
-<dd>Lauren</dd>
-<dd>Andrea</dd>
-<dd>Jason</dd>
-<dd>Jasper</dd>
-<dd>Bryson</dd>
-<dd>Caity</dd>
-
-
-</div><div><dt>Solve Jake's puzzle</dt>
-<dd>Lauren</dd>
-<dd>Jake</dd>
-<dd>Andrea</dd>
-<dd>Bryson</dd>
-<dd>Megan</dd>
-<dd>Eric</dd>
-
-
-</div><div><dt>Go skinny dipping</dt>
-<dd>Lauren</dd>
-<dd>Jason</dd>
-<dd>Celeste</dd>
-<dd>Megan</dd>
-<dd>Mary</dd>
-
-
-</div><div><dt>Go vegan for two weeks</dt>
+<div>
+<dt>Get an astrology or tarot reading done by a professional</dt>
 <dd>Lauren</dd>
 <dd>Andrea</dd>
 <dd>Jamie</dd>
 <dd>Celeste</dd>
-<dd>Megan</dd>
-<dd>Eric</dd>
+</div>
 
 
-</div><div><dt>Eat a bug, intentionally</dt>
-<dd>Lauren</dd>
-<dd>Jake</dd>
-
-
-</div><div><dt>Master a classic cocktail</dt>
-<dd>Lauren</dd>
-<dd>Mary</dd>
-<dd>Ellie</dd>
-<dd>Bryon</dd>
-<dd>Eric</dd>
-
-
-</div><div><dt>Disable all social media accounts for a week</dt>
+<div>
+<dt>Participate in a sun dress bar crawl</dt>
 <dd>Lauren</dd>
 <dd>Jake</dd>
 <dd>Jasper</dd>
+<dd>Megan</dd>
+</div>
+
+
+<div>
+<dt>Attend or volunteer at an event with a queer allied organization</dt>
+<dd>Lauren</dd>
+<dd>Andrea</dd>
+<dd>Jason</dd>
+<dd>Jasper</dd>
+<dd>Bryson</dd>
+<dd>Caity</dd>
+</div>
+
+
+<div>
+<dt>Solve Jake's puzzle</dt>
+<dd>Lauren</dd>
+<dd>Jake</dd>
+<dd>Andrea</dd>
+<dd>Bryson</dd>
+<dd>Megan</dd>
+<dd>Eric</dd>
+</div>
+
+
+<div>
+<dt>Go skinny dipping</dt>
+<dd>Lauren</dd>
+<dd>Jason</dd>
+<dd>Celeste</dd>
+<dd>Megan</dd>
+<dd>Mary</dd>
+</div>
+
+
+<div>
+<dt>Go vegan for two weeks</dt>
+<dd>Lauren</dd>
+<dd>Andrea</dd>
+<dd>Jamie</dd>
+<dd>Celeste</dd>
+<dd>Megan</dd>
+<dd>Eric</dd>
+</div>
+
+
+<div>
+<dt>Eat a bug, intentionally</dt>
+<dd>Lauren</dd>
+<dd>Jake</dd>
+</div>
+
+
+<div>
+<dt>Master a classic cocktail</dt>
+<dd>Lauren</dd>
+<dd>Mary</dd>
+<dd>Ellie</dd>
+<dd>Bryon</dd>
+<dd>Eric</dd>
+</div>
+
+
+<div>
+<dt>Disable all social media accounts for a week</dt>
+<dd>Lauren</dd>
+<dd>Jake</dd>
+<dd>Jasper</dd>
 <dd>Bryson</dd>
 <dd>Megan</dd>
 <dd>Ellie</dd>
+</div>
 
 
-</div><div><dt>Memorize and recite your favorite poem to five BAABO players</dt>
+<div>
+<dt>Memorize and recite your favorite poem to five BAABO players</dt>
 <dd>Lauren</dd>
 <dd>Jamie</dd>
 <dd>Bryon</dd>
+</div>
 
 
-</div><div><dt>Make a new to you cocktail or mocktail with 5+ ingredients and share with 3+ players</dt>
+<div>
+<dt>Make a new to you cocktail or mocktail with 5+ ingredients and share with 3+ players</dt>
 <dd>Jake</dd>
 <dd>Jamie</dd>
 <dd>Jasper</dd>
 <dd>Bryson</dd>
 <dd>Eric</dd>
+</div>
 
 
-</div><div><dt>Sing a song at a local karaoke bar</dt>
+<div>
+<dt>Sing a song at a local karaoke bar</dt>
 <dd>Jake</dd>
 <dd>Andrea</dd>
 <dd>Celeste</dd>
 <dd>Bryson</dd>
 <dd>Mary</dd>
 <dd>Eric</dd>
+</div>
 
 
-</div><div><dt>Enter something into the Northwest Washington Fair</dt>
+<div>
+<dt>Enter something into the Northwest Washington Fair</dt>
 <dd>Jake</dd>
 <dd>Caity</dd>
+<dd>Julie</dd>
+</div>
 
 
-</div><div><dt>Take a picture of a bald eagle</dt>
+<div>
+<dt>Take a picture of a bald eagle</dt>
 <dd>Jake</dd>
 <dd>Jason</dd>
 <dd>Jamie</dd>
 <dd>Jasper</dd>
 <dd>Bryson</dd>
 <dd>Bryon</dd>
+</div>
 
 
-</div><div><dt>Go bowling and get a strike</dt>
+<div>
+<dt>Go bowling and get a strike</dt>
 <dd>Jake</dd>
 <dd>Andrea</dd>
 <dd>Jason</dd>
 <dd>Jasper</dd>
 <dd>Bryon</dd>
+<dd>Julie</dd>
+</div>
 
 
-</div><div><dt>Buy something weird at a yard sale or street fair</dt>
+<div>
+<dt>Buy something weird at a yard sale or street fair</dt>
 <dd>Jake</dd>
 <dd>Jason</dd>
 <dd>Jamie</dd>
 <dd>Jasper</dd>
 <dd>Megan</dd>
+</div>
 
 
-</div><div><dt>Write an original song and perform it for the group or at an open mic</dt>
+<div>
+<dt>Write an original song and perform it for the group or at an open mic</dt>
 <dd>Jake</dd>
 <dd>Andrea</dd>
 <dd>Jason</dd>
 <dd>Jamie</dd>
 <dd>Bryon</dd>
+</div>
 
 
-</div><div><dt>Go do something spooky at Bayview cemetary</dt>
+<div>
+<dt>Go do something spooky at Bayview cemetary</dt>
 <dd>Jake</dd>
 <dd>Jason</dd>
+</div>
 
 
-</div><div><dt>Make a toy, or write a childhood song or story</dt>
+<div>
+<dt>Make a toy, or write a childhood song or story</dt>
 <dd>Jake</dd>
 <dd>Megan</dd>
 <dd>Ellie</dd>
+</div>
 
 
-</div><div><dt>Get it in the hole in a game of cornhole</dt>
+<div>
+<dt>Get it in the hole in a game of cornhole</dt>
 <dd>Jake</dd>
 <dd>Andrea</dd>
 <dd>Celeste</dd>
+</div>
 
 
-</div><div><dt>Get a new hairdo (or facial hair)</dt>
+<div>
+<dt>Get a new hairdo (or facial hair)</dt>
 <dd>Jake</dd>
 <dd>Jason</dd>
 <dd>Caity</dd>
+<dd>Julie</dd>
+</div>
 
 
-</div><div><dt>Help three people check off a square or join them</dt>
+<div>
+<dt>Help three people check off a square or join them</dt>
 <dd>Jake</dd>
 <dd>Ellie</dd>
 <dd>Bryon</dd>
+</div>
 
 
-</div><div><dt>Drink a mai tai on a beach</dt>
+<div>
+<dt>Drink a mai tai on a beach</dt>
 <dd>Jake</dd>
 <dd>Andrea</dd>
 <dd>Jasper</dd>
@@ -430,9 +536,11 @@ dt {
 <dd>Mary</dd>
 <dd>Caity</dd>
 <dd>Bryon</dd>
+</div>
 
 
-</div><div><dt>Go to Downtown Sounds</dt>
+<div>
+<dt>Go to Downtown Sounds</dt>
 <dd>Jake</dd>
 <dd>Jamie</dd>
 <dd>Mary</dd>
@@ -440,9 +548,12 @@ dt {
 <dd>Caity</dd>
 <dd>Bryon</dd>
 <dd>Eric</dd>
+<dd>Julie</dd>
+</div>
 
 
-</div><div><dt>See a Bells win</dt>
+<div>
+<dt>See a Bells win</dt>
 <dd>Jake</dd>
 <dd>Jason</dd>
 <dd>Jasper</dd>
@@ -451,28 +562,37 @@ dt {
 <dd>Megan</dd>
 <dd>Mary</dd>
 <dd>Bryon</dd>
+<dd>Julie</dd>
+</div>
 
 
-</div><div><dt>Have an adult tea party</dt>
+<div>
+<dt>Have an adult tea party</dt>
 <dd>Jake</dd>
+</div>
 
 
-</div><div><dt>Touch snow</dt>
+<div>
+<dt>Touch snow</dt>
 <dd>Andrea</dd>
 <dd>Celeste</dd>
 <dd>Megan</dd>
 <dd>Ellie</dd>
 <dd>Eric</dd>
+</div>
 
 
-</div><div><dt>Take a picture of a live hummingbrid</dt>
+<div>
+<dt>Take a picture of a live hummingbrid</dt>
 <dd>Andrea</dd>
 <dd>Bryson</dd>
 <dd>Mary</dd>
 <dd>Bryon</dd>
+</div>
 
 
-</div><div><dt>Complete a self-care thing you've been putting off</dt>
+<div>
+<dt>Complete a self-care thing you've been putting off</dt>
 <dd>Andrea</dd>
 <dd>Jamie</dd>
 <dd>Megan</dd>
@@ -480,203 +600,284 @@ dt {
 <dd>Caity</dd>
 <dd>Bryon</dd>
 <dd>Eric</dd>
+</div>
 
 
-</div><div><dt>Make a difficult dessert</dt>
+<div>
+<dt>Make a difficult dessert</dt>
 <dd>Andrea</dd>
 <dd>Celeste</dd>
 <dd>Ellie</dd>
+<dd>Julie</dd>
+</div>
 
 
-</div><div><dt>Extreme chacuterie (pack and hike a board into the wilderness)</dt>
+<div>
+<dt>Extreme chacuterie (pack and hike a board into the wilderness)</dt>
 <dd>Andrea</dd>
 <dd>Jamie</dd>
 <dd>Caity</dd>
+<dd>Julie</dd>
+</div>
 
 
-</div><div><dt>Beat Jasper in pickleball</dt>
+<div>
+<dt>Beat Jasper in pickleball</dt>
 <dd>Andrea</dd>
 <dd>Jamie</dd>
 <dd>Mary</dd>
 <dd>Ellie</dd>
+</div>
 
 
-</div><div><dt>Conduct a science experiment and present your findings to the group</dt>
+<div>
+<dt>Conduct a science experiment and present your findings to the group</dt>
 <dd>Andrea</dd>
 <dd>Mary</dd>
 <dd>Eric</dd>
+</div>
 
 
-</div><div><dt>Treat yourself</dt>
+<div>
+<dt>Treat yourself</dt>
 <dd>Andrea</dd>
 <dd>Jamie</dd>
 <dd>Ellie</dd>
+</div>
 
 
-</div><div><dt>Spend an evening on the Bay</dt>
+<div>
+<dt>Spend an evening on the Bay</dt>
 <dd>Andrea</dd>
 <dd>Jamie</dd>
 <dd>Bryon</dd>
 <dd>Eric</dd>
+</div>
 
 
-</div><div><dt>Walk onto a ferry and have a meal on an island of your choosing</dt>
+<div>
+<dt>Walk onto a ferry and have a meal on an island of your choosing</dt>
 <dd>Jason</dd>
 <dd>Eric</dd>
+</div>
 
 
-</div><div><dt>Learn to do an impression (of a Bellebrity?)</dt>
+<div>
+<dt>Learn to do an impression (of a Bellebrity?)</dt>
 <dd>Jason</dd>
 <dd>Celeste</dd>
 <dd>Bryson</dd>
 <dd>Ellie</dd>
 <dd>Caity</dd>
+<dd>Julie</dd>
+</div>
 
 
-</div><div><dt>Do a chore for a friend (their choice)</dt>
+<div>
+<dt>Do a chore for a friend (their choice)</dt>
 <dd>Jason</dd>
 <dd>Jasper</dd>
 <dd>Megan</dd>
 <dd>Eric</dd>
+<dd>Julie</dd>
+</div>
 
 
-</div><div><dt>Get narcan and learn (or teach) when and how to use it</dt>
+<div>
+<dt>Get narcan and learn (or teach) when and how to use it</dt>
 <dd>Jason</dd>
 <dd>Jamie</dd>
 <dd>Mary</dd>
+</div>
 
 
-</div><div><dt>Hike 50 miles cumulatively</dt>
+<div>
+<dt>Hike 50 miles cumulatively</dt>
 <dd>Jason</dd>
 <dd>Jasper</dd>
 <dd>Celeste</dd>
 <dd>Bryson</dd>
 <dd>Megan</dd>
 <dd>Bryon</dd>
+<dd>Julie</dd>
+</div>
 
 
-</div><div><dt>Make an edit on Wikipedia that is accepted</dt>
+<div>
+<dt>Make an edit on Wikipedia that is accepted</dt>
 <dd>Jamie</dd>
 <dd>Jasper</dd>
 <dd>Bryson</dd>
+</div>
 
 
-</div><div><dt>Read a book of fiction</dt>
+<div>
+<dt>Read a book of fiction</dt>
 <dd>Jamie</dd>
 <dd>Celeste</dd>
 <dd>Eric</dd>
+</div>
 
 
-</div><div><dt>Read a book in a genre you haven't read before</dt>
+<div>
+<dt>Read a book in a genre you haven't read before</dt>
 <dd>Jamie</dd>
 <dd>Megan</dd>
+<dd>Julie</dd>
+</div>
 
 
-</div><div><dt>Paint your partner's nails (or a friend)</dt>
+<div>
+<dt>Paint your partner's nails (or a friend)</dt>
 <dd>Jamie</dd>
 <dd>Ellie</dd>
 <dd>Eric</dd>
+<dd>Julie</dd>
+</div>
 
 
-</div><div><dt>Brunch on a patio</dt>
+<div>
+<dt>Brunch on a patio</dt>
 <dd>Jamie</dd>
 <dd>Jasper</dd>
 <dd>Celeste</dd>
 <dd>Bryon</dd>
 <dd>Eric</dd>
+<dd>Julie</dd>
+</div>
 
 
-</div><div><dt>Swim in a new to you natural body of water</dt>
+<div>
+<dt>Swim in a new to you natural body of water</dt>
 <dd>Jamie</dd>
 <dd>Bryson</dd>
 <dd>Ellie</dd>
+</div>
 
 
-</div><div><dt>Play hot stick and be in the top three</dt>
+<div>
+<dt>Play hot stick and be in the top three</dt>
 <dd>Jasper</dd>
 <dd>Celeste</dd>
+</div>
 
 
-</div><div><dt>Attend a group bike ride</dt>
+<div>
+<dt>Attend a group bike ride</dt>
 <dd>Celeste</dd>
 <dd>Mary</dd>
 <dd>Caity</dd>
+</div>
 
 
-</div><div><dt>Visit the Bellingham touch tank and learn a fact to share</dt>
+<div>
+<dt>Visit the Bellingham touch tank and learn a fact to share</dt>
 <dd>Celeste</dd>
 <dd>Caity</dd>
+</div>
 
 
-</div><div><dt>Receive recognition at bar trivia</dt>
+<div>
+<dt>Receive recognition at bar trivia</dt>
 <dd>Celeste</dd>
 <dd>Megan</dd>
+<dd>Julie</dd>
+</div>
 
 
-</div><div><dt>Identify and hug five native tree species</dt>
+<div>
+<dt>Identify and hug five native tree species</dt>
 <dd>Celeste</dd>
 <dd>Bryson</dd>
 <dd>Mary</dd>
 <dd>Eric</dd>
+<dd>Julie</dd>
+</div>
 
 
-</div><div><dt>Find five Fairhaven historic markers</dt>
+<div>
+<dt>Find five Fairhaven historic markers</dt>
 <dd>Celeste</dd>
 <dd>Bryson</dd>
 <dd>Megan</dd>
 <dd>Ellie</dd>
+<dd>Julie</dd>
+</div>
 
 
-</div><div><dt>Host a game night</dt>
+<div>
+<dt>Host a game night</dt>
 <dd>Bryson</dd>
+</div>
 
 
-</div><div><dt>Attend a Bellingham Pride event</dt>
+<div>
+<dt>Attend a Bellingham Pride event</dt>
 <dd>Bryson</dd>
+</div>
 
 
-</div><div><dt>Plan an execute a romatic date with a friend</dt>
+<div>
+<dt>Plan an execute a romatic date with a friend</dt>
 <dd>Megan</dd>
 <dd>Ellie</dd>
+</div>
 
 
-</div><div><dt>Have a picnic</dt>
+<div>
+<dt>Have a picnic</dt>
 <dd>Megan</dd>
 <dd>Mary</dd>
+</div>
 
 
-</div><div><dt>Observe and identify at least three new birds</dt>
+<div>
+<dt>Observe and identify at least three new birds</dt>
 <dd>Megan</dd>
+</div>
 
 
-</div><div><dt>Attend or host an outdoor movie viewing</dt>
+<div>
+<dt>Attend or host an outdoor movie viewing</dt>
 <dd>Mary</dd>
 <dd>Caity</dd>
 <dd>Bryon</dd>
+</div>
 
 
-</div><div><dt>Chateau St. Michelle concert</dt>
+<div>
+<dt>Chateau St. Michelle concert</dt>
 <dd>Mary</dd>
+</div>
 
 
-</div><div><dt>Wine tasting at an actual winery or vineyard</dt>
+<div>
+<dt>Wine tasting at an actual winery or vineyard</dt>
 <dd>Ellie</dd>
 <dd>Caity</dd>
 <dd>Bryon</dd>
+</div>
 
 
-</div><div><dt>Visit a new to you museum</dt>
+<div>
+<dt>Visit a new to you museum</dt>
 <dd>Ellie</dd>
 <dd>Eric</dd>
+</div>
 
 
-</div><div><dt>Play pickleball at Cornwall Park</dt>
+<div>
+<dt>Play pickleball at Cornwall Park</dt>
 <dd>Caity</dd>
+<dd>Julie</dd>
+</div>
 
 
-</div><div><dt>Go dancing</dt>
+<div>
+<dt>Go dancing</dt>
 <dd>Bryon</dd>
 <dd>Eric</dd>
+</div>
 
 </dl>
 </div>
