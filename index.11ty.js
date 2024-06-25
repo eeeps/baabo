27b29aab4793b225160ab15c474f8fb3f6054f8d
Eric Portis <e@ericportis.com>
2024-06-24 21:02:22 -0700

    Fix spacing on index

--- index.11ty.js
+++ index.11ty.js
@@ -12,9 +12,7 @@ exports.render = function(data) {
 	<h2>BAABO</h2>
 	
 	<ul class="years">
-		<li><a href="summer2024">
-			<span style="display: block; margin-top: -0.25em;">2024</span>
-		</a></li>
+		<li><a href="summer2024">2024</a></li>
 		<li><a href="winter2023-24">
 			<span style="display: block; font-size: 0.625em;">Winter</span>
 			<span style="display: block; font-size: 0.45em; margin-top: -0.33em;">2023-2024</span>
