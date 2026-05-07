c55e070e44e8044fcb5313b06ad72c758f62f5c4
Eric Portis <e@ericportis.com>
2026-05-07 13:37:37 -0700

    Prize checkbox interactivity
    make them bigger on phones, too

--- prizes.11ty.js
+++ prizes.11ty.js
@@ -22,7 +22,7 @@ export const data = {
 export function render(data) {
 			
 	return `
-<div class="mainContain prizes" style="display: grid; grid-template-columns: 8rem 1fr;">
+<div class="mainContain prizes prizesDetail" style="display: grid; grid-template-columns: 8rem 1fr;">
 	<p style="grid-column: 1/3; text-align: center; font-size: 2rem;"><a href="/${ urlSlugify( data.game.name ) }/#prize-${ urlSlugify( data.prize.what ) }">← Back</a></p>
 	<p class="emoji" style="text-align: center; view-transition-name: prize-${ urlSlugify( data.prize.what ) }-emoji">${ monochromize( data.prize.emoji ) }</p>
 	<div>
@@ -47,8 +47,7 @@ export function render(data) {
 				<dt
 					style="view-transition-name: prize-${ urlSlugify( data.prize.what ) }-won-by-dt"
 				>Won by</dt>
-				<dd><form style='display: grid; grid-template-columns: repeat( auto-fit, minmax(10ch, 1fr) );'
-				  ${ ( remainingPrizeCount( data.prize ) <= 0 ? 'class=noneAvailable' : '' ) }
+				<dd><form style='display: grid; grid-template-columns: repeat( auto-fit, minmax(15ch, 1fr) );'
 				  ${ ( remainingPrizeCount( data.prize ) !== null && remainingPrizeCount( data.prize ) <= 0 ? 'class=noneAvailable' : '' ) }
 				>
 				${ data.players.map( player => { 
@@ -76,6 +75,53 @@ export function render(data) {
 
 </div>
 
+<script>
+const maxWinners = ${ data.prize.maxWinners };
+const checkboxes = document.querySelectorAll('input[type=checkbox]');
+checkboxes.forEach( c => {
+	
+	const checkedNow = [...document.querySelectorAll('input[type=checkbox]')]
+		.reduce( (acc, cv) => {
+			if ( cv.checked ) { acc += 1; } 
+			return acc;
+		}, 0 );
+	console.log(checkedNow);
+	if ( maxWinners === null || checkedNow < maxWinners || c.checked ) {
+		c.removeAttribute('disabled');
+	}
+	
+	c.addEventListener( 'input', ( event ) => {
+		
+		// update the dom so our css works
+		if ( event.target.checked ) {
+			event.target.setAttribute('checked', 'checked')
+		} else {
+			event.target.removeAttribute('checked');
+		}
+		
+		const checkedNow = [...document.querySelectorAll('input[type=checkbox]')]
+			.reduce( (acc, cv) => {
+				if ( cv.checked ) { acc += 1; } 
+				return acc;
+			}, 0 );
+
+		if ( maxWinners !== null && checkedNow >= maxWinners ) {
+			[...document.querySelectorAll( 'input[type=checkbox]:not([checked])' ) ]
+				.filter( d => d !== event.target )
+				.forEach( d => {
+					d.setAttribute('disabled', 'disabled');
+				} )
+			event.target.form.classList.add('noneAvailable');
+		} else {
+			[...document.querySelectorAll( 'input[type=checkbox]' ) ]
+				.forEach( d => {
+					d.removeAttribute('disabled')
+				} )	
+			event.target.form.classList.remove('noneAvailable');
+		}
+	} );
+} );
+</script>
 `;
 
 };
